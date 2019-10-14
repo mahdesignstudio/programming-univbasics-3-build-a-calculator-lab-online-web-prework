2097441a1fb70d79b633ae6b05f2af903502ca25
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-10-14 02:15:01 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,37 @@
 def addition(num1, num2)
-
+puts "hi"
+num2 + num1 
 end
+addition(2,1)
+
 
 def subtraction(num1, num2)
-
+puts "hola"
+num1 - num2 
 end
+subtraction(10,2)
 
 def division(num1, num2)
-
+puts "Bonjour"
+num1 / num2 
 end
+division(10,2)
 
 def multiplication(num1, num2)
-
+puts "Ciao"
+num1 * num2 
 end
+multiplication(10,2)
 
 def modulo(num1, num2)
-
+puts "Wepa"
+num1 % num2
 end
+modulo(5,2)
 
 def square_root(num)
-
+puts "fin"
+num.sqrt
 end
+square_root(64)
+
